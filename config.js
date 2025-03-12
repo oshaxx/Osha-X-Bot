c0fde74f1461567bd6cfe118c172e366f81eaf28
OshaX <87685400+oshaxx@users.noreply.github.com>
2025-03-12 15:51:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/oshaxx/imag/refs/heads/main/file.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello... I'm Osha x Bot !!! ",
 };
