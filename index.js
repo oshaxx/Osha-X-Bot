0a4445c8616779784655efb68f826bf5886e75e1
OshaX <87685400+oshaxx@users.noreply.github.com>
2025-03-12 15:54:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
 let up = `Osha x BOT connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://github.com/oshaxx/imag/blob/main/file.jpg?raw=true` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://raw.githubusercontent.com/oshaxx/imag/refs/heads/main/file.jpg` }, caption: up })
 
 }
 })
